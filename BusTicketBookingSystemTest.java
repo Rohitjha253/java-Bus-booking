9ac33a2b666a2ee6973856909ebdff77d567c848
Rohit Jha <jharohit25@gmail.com>
2023-05-08 10:22:48 +0530

    latest commit

--- BusTicketBookingSystemTest.java
+++ BusTicketBookingSystemTest.java
@@ -1,35 +1,25 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.beans.Transient;
 import java.util.Arrays;
 import java.util.List;
 public class BusTicketBookingSystemTest {
     private BusTicketBookingSystem system;
-    @BeforeEach
-    void setUp() {
-        system = new BusTicketBookingSystem();
-        List<String> seats1 = Arrays.asList("1A", "1B", "2A", "2B");
-        system.addBus("Bus 1", seats1);
-        List<String> seats2 = Arrays.asList("1C", "1D", "2C", "2D");
-        system.addBus("Bus 2", seats2);
-    }
-    @Test
-    void testBookSeat() {
-        system.bookSeat("Bus 1", "1A");
-        List<String> availableSeats = system.getAvailableSeats("Bus 1");
-        Assertions.assertEquals(Arrays.asList("1B", "2A", "2B"), availableSeats);
-    }
-    @Test
-    void testBookSeatNotFound() {
-        system.bookSeat("Bus 1", "3A");
-        List<String> availableSeats = system.getAvailableSeats("Bus 1");
-        Assertions.assertEquals(Arrays.asList("1A", "1B", "2A", "2B"), availableSeats);
-    }
-    @Test
-    void testPrintAvailableSeats() {
-        String expectedOutput = "Available seats for Bus 1: 1A, 1B, 2A, 2B";
-        Assertions.assertEquals(expectedOutput, system.printAvailableSeats("Bus 1", null));
-    }
+    
+    
+    /* @Test
+    void testBooking(){
+        assertEquals(6, BusBookingSystem.bookTicket("Kolkata","Delhi"));
+    } */
 
-
+    @Test(expected = InvalidBookingException.class)
+    public void testInvalidSource() throws InvalidBookingException {
+        BusBookingSystem busBookingSystem = new BusBookingSystem();
+        busBookingSystem.bookTicket("Delhi", "Mumbai");
+        // Enter an invalid source.
+        System.setIn(new java.io.ByteArrayInputStream("123\n".getBytes()));
+        busBookingSystem.bookTicket("", "Mumbai");
+    }
 }
